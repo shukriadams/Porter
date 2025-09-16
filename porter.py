faf69e62b9b721cc6e1ba8dd1ee771c0e4efc495
Shukri Adams <shukri.adams@gmail.com>
2025-09-17 00:05:01 +0200

    Update porter.py
    added runtime checks, simplified package entries to a simple string

--- porter.py
+++ porter.py
@@ -1,3 +1,5 @@
+# Proof-of-concept for Porter, a packages-as-source-code manager for C#.
+
 # porter --install /some/path
 import argparse
 import os
@@ -8,12 +10,22 @@ import shutil
 import subprocess
 import glob
 import codecs
+import uuid
+import base64
+import shutil
 
 class Package:
     def __init__(self, repo, tag):
         self.Source = 'https://github.com' # currently only public github repos supported
         self.Repo = repo.replace('.', '/') # github repos dont have .'s, must be /
         self.Tag = tag # current only only tag is supported
+
+# file-safe encoding (base64)
+def encode(str):
+    b = str.encode('ascii')
+    b64_bytes = base64.b64encode(b)
+    s = b64_bytes.decode('ascii')
+    return s.replace('/', '_')
 
 def exec(command, cwd=None):
 
@@ -55,12 +67,10 @@ def exec(command, cwd=None):
     return returnCode
 
 
-def process_porter(root_dir_path, context=[]):
+def process_porter(root_dir_path, context=[], require_run_times=None):
+    
+    # load the porter.json in this directory, we already confirmed it's here
     porter_start_file_path = os.path.join(root_dir_path, 'porter.json')
-    if not os.path.isfile(porter_start_file_path):
-        print(f'Error : expected porter.json not found at path {root_dir_path}, ignoring')
-        return
-
     porter_conf = {}
     porter_packages_dir = os.path.join(root_dir_path, 'porter')
 
@@ -73,24 +83,50 @@ def process_porter(root_dir_path, context=[]):
 
     packages_to_install = []
     package_name=porter_conf['name']
-    
-    context = context[:]
-    context.append(package_name)
 
-    print(f'!! PORTER processing path {root_dir_path}, context is {context}')
+    # enforce required structures in conf
+    if porter_conf['runtimes'] == None:
+        porter_conf['runtimes'] = []
+
+    if porter_conf['packages'] == None:
+        porter_conf['packages'] = []
+    
+    # if runtimes not set, we're at top-level project, use it's runtimes
+    if require_run_times == None:
+        if len(porter_conf['runtimes']) == 0:
+            print('top level project must declare at least one expected runtime')
+            sys.exit(1)
+        
+        require_run_times = porter_conf['runtimes']
+
+    context = context[:] # copy array so we don't end up cascading additions to adjacent recursive calls
+    context.append(package_name)
 
     # generate package to install from conf packages
     for package in porter_conf['packages']:
-        src = package['source']
-        if package['source'] != 'github':
-            print(f'Error : only github currently supported : {src}') 
+        # get package source from package string. format is expected to be 
+        # source.author+repo@tag
+        parts = regex.search('([^.]*).(.*)@(.*)', package)
+        if not parts.groups:
+            print(f'package {package} is malformed')
+            continue
+
+        if not parts.group(1) or not parts.group(2) or not parts.group(3):
+            print(f'package {package} is malformed')
+            continue
+            
+        package_source = parts.group(1)
+        auth_repo = parts.group(2)
+        tag = parts.group(3)
+
+        if package_source != 'github':
+            print(f'Error : only github currently supported : {package_source}') 
             sys.exit(1)
 
-        p = Package(package['repo'], package['tag'])
+        p = Package(auth_repo, tag)
         packages_to_install.append(p)
 
     for package in packages_to_install:
-        print(package.Repo)
         occurences = [i for i, x in enumerate(packages_to_install) if x.Repo == package.Repo]
         if len(occurences) > 1:
             print(f'Error : package {package.Repo} occurs more than once')
@@ -102,35 +138,68 @@ def process_porter(root_dir_path, context=[]):
     destructive=True
 
     for package in packages_to_install:
-        package_dir = os.path.join(porter_packages_dir, package.Repo)
-        if destructive and os.path.isdir(package_dir):
-            shutil.rmtree(package_dir)   
+        # create temp dir in porter's own work dir, use deterministic path so we always
+        # clean up after ourselves. Path is based on package's total namespace depth, so
+        # no chance of collision with other packages. make it filesystem safe by safe 
+        # base64 encoding. Before cloning always delete the temp path
+        package_temp_dir = os.path.join(work_dir, encode('_'.join(context)))
 
-        if not os.path.isdir(package_dir):
-            os.makedirs(package_dir)
-            result = exec(f'git clone --branch {package.Tag} {package.Source}/{package.Repo} {package_dir}')
-            if result != 0:
-                sys.exit(0)
+        # convert to abs path for remapping later
+        package_temp_dir = os.path.abspath(package_temp_dir)
+
+        if os.path.isdir(package_temp_dir):
+            shutil.rmtree(package_temp_dir)
+        os.makedirs(package_temp_dir)
+
+        # clone package to temp location, we need to analyse it first
+        result = exec(f'git clone --branch {package.Tag} {package.Source}/{package.Repo} {package_temp_dir}')
+        if result != 0:
+            sys.exit(0)
         
-        package_porter_conf = os.path.join(package_dir, 'porter.json')
+        # package must have a porter.json file in it, else we ignore it
+        package_porter_conf = os.path.join(package_temp_dir, 'porter.json')
         isPackagePorter=True
         if not os.path.isfile(package_porter_conf):
             print(f'Warning : package @ path {package_porter_conf} is not a porter package')
-            isPackagePorter=False        
+            continue
 
-        # delete .git dir
-        git_dir = os.path.join(package_dir, '.git')
-        if os.path.isdir(git_dir):
-            shutil.rmtree(git_dir)
+        # load porter.json
+        try:
+            with open(package_porter_conf) as file:
+                this_package_porter_conf = json.load(file)
+        except Exception as e:
+            print(f'Error : loading json from {package_porter_conf}. Assuming not a valid porter package, will not proces. {e}')
+            continue
 
-        # wrap all cs files
-        cs_files = glob.glob(os.path.join(package_dir, '**.cs')) # not sure this will work on nested
+        this_package_name = this_package_porter_conf['name']
+
+        # enforce top level runtimes on this
+        this_package_runtimes = this_package_porter_conf['runtimes']
+        if not any(runtimes in set(this_package_runtimes) for runtimes in require_run_times):
+            print(f'{this_package_name} runtimes {this_package_runtimes} do not align with required runtimes {require_run_times}')
+            sys.exit(1)
+
+        # destroy then create public directory of this package
+        child_package_dir = os.path.join(porter_packages_dir, this_package_name)
+        # convert to absolute for remapping
+        child_package_dir = os.path.abspath(child_package_dir)
+
+        if destructive and os.path.isdir(child_package_dir):
+            shutil.rmtree(child_package_dir)   
+
+        if not os.path.isdir(child_package_dir):
+            os.makedirs(child_package_dir)
+
+        # find all .cs files in package temp, we want to wrap and copy them
+        cs_files = glob.glob(os.path.join(package_temp_dir, '**.cs')) # not sure this will work on nested
         for cs_file in cs_files:
+            # convert to abs path for easier remap
+            cs_file = os.path.abspath(cs_file)
+
             with codecs.open(cs_file, encoding='utf-8') as file:
                 file_content = file.read()
-                if ('//PORTER-WRAPPER' in file_content):
-                    continue
 
+                # wrap file contents in namespace stack threaded down package stack 
                 namespace_lead = '//PORTER-WRAPPER!\n'
                 namespace_tail= ''
                 for this_context in context:
@@ -141,18 +210,29 @@ def process_porter(root_dir_path, context=[]):
 
                 namespace_lead = f'{namespace_lead}//PORTER-WRAPPER!\n\n\n'
                 namespace_tail = f'\n\n//PORTER-WRAPPER!\n{namespace_tail}//PORTER-WRAPPER!'
-
-
                 file_content = f'{namespace_lead}{file_content}{namespace_tail}'
 
-                with open(cs_file, 'w') as output:
+                # remap .cs file in temp dir to public child package dir
+                remapped_file_path = cs_file.replace(package_temp_dir, child_package_dir)
+
+                with open(remapped_file_path, 'w') as output:
                     output.write(file_content)
-                    print(f'wrapped file {cs_file}')
 
-        if isPackagePorter:
-            process_porter(package_dir, context)   
+        # copy porter.json over too, for reference. we probably want to add some of our own stuff to this later ala npm
+        shutil.copyfile(os.path.join(package_temp_dir, 'porter.json'), 
+            os.path.join(child_package_dir, 'porter.json'))
+
+        # clean up temp child package dir
+        shutil.rmtree(package_temp_dir)
+
+        print(f'Installed package {this_package_name}')
+
+        # finally recurse by running in child package dir,
+        process_porter(child_package_dir, context)   
 
 
+# let's get started - currently the only supported call is  --install some/path
+print('Porter, a package manager for C#')
 
 argsParser = argparse.ArgumentParser()
 argsParser.add_argument('--install', default=None)
@@ -162,13 +242,22 @@ if args.install is None:
     print('Error : try --install <some-path>')
     sys.exit(1)
 
-print(f'installing in {args.install}')
+print(f'Running in dir : {args.install}')
 
+# the directory we start processing in must have a porter.json file in it.
 root_dir_path = os.path.abspath(args.install)
-
 porter_start_file_path = os.path.join(root_dir_path, 'porter.json')
 if not os.path.isfile(porter_start_file_path):
     print(f'Error : expected porter.json not found at path {root_dir_path}')
     sys.exit(1)
 
+# create a work dir for porter, it needs this to check incoming git checkouts
+# before merging their contents into top-level packages directory
+work_dir = os.path.join(os.getcwd(), '.porter_temp')
+if not os.path.isdir(work_dir):
+    os.makedirs(work_dir)
+
+# recurse through all the packages, all the way down through all the packages
 process_porter(root_dir_path)
+
+print('Done!')
